ef0d5baa462668e8bfb406f67dadfe39a4d850c7
EricM76 <53233372+EricM76@users.noreply.github.com>
2021-08-30 18:29:19 -0300

    agrega session & middlewares

--- routes/main.js
+++ routes/main.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const {index, search, admin, addBanner,allBanner,storeBanner} = require('../controllers/mainController');
 
 const upload = require('../middlewares/imageBannerStorage');
+const adminUserCheck = require('../middlewares/adminUserCheck');
 
 router.get('/',index);
 router.get('/search',search);
-router.get('/admin',admin);
-router.get('/admin/banner/add',addBanner);
+router.get('/admin', adminUserCheck, admin);
+router.get('/admin/banner/add',adminUserCheck ,addBanner);
 router.post('/admin/banner/add',upload.single('image'), storeBanner)
-router.get('/admin/banner/all',allBanner);
+router.get('/admin/banner/all',adminUserCheck,allBanner);
 
 module.exports =router;
